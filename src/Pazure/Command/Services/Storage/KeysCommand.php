36ae726b521953a22b918c6e28bd9a78ec596cab
Gordon Franke <info@nevalon.de>
2012-06-29 11:17:06 +0200

    ad storage keys command

---
+++ src/Pazure/Command/Services/Storage/KeysCommand.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of Pazure.
+ *
+ * (c) Gordon Franke <info@nevalon.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Pazure\Command\Services\Storage;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Cilex\Command\Command;
+
+use Exception;
+
+/**
+ * The Get Storage Keys operation returns the primary and secondary access keys for the specified storage account.
+ *
+ * @author Gordon Franke <info@nevalon.de>
+ */
+class KeysCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('services:storage:keys')
+            ->setDescription('List storage service account keys')
+            ->setDefinition(array(
+                new InputArgument(
+                    'service_name', InputArgument::REQUIRED
+                )
+            ));
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $serviceName = $input->getArgument('service_name');
+
+        $command = $this->getService('guzzle')
+            ->get('azure')
+            ->getCommand('services.storage.keys', array('service_name' => $serviceName));
+        $storageAccountKeys = $command->execute();
+
+        $output->writeln(sprintf('<comment>Service name: %s</comment>', $serviceName));
+        $output->writeln(sprintf('Url: %s', $storageAccountKeys->Url));
+        $output->writeln(sprintf('Primary: %s', $storageAccountKeys->StorageServiceKeys->Primary));
+        $output->writeln(sprintf('Secondary: %s', $storageAccountKeys->StorageServiceKeys->Secondary));
+    }
+}
